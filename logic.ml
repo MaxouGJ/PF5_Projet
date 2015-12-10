7f1ae77c64bbaaed94478f787d4bc7d46eefc9cf
MaxouGJ <mgallaisj@gmail.com>
2015-12-10 23:23:14 +0100

    Première partie de la cnf il reste à mettre dans un fichier

--- logic.ml
+++ logic.ml
@@ -8,8 +8,64 @@ type formula = True
 		|Or of formula * formula
 		|Neg of formula;;
 
-(*Fonctions annexes*)
+(*Règles de bases*)
+let baseRules = ref [(Alive,Alive,Alive,Alive,Alive);
+		 (Alive,Alive,Alive,Dead,Alive);
+		 (Alive,Alive,Dead,Alive,Alive);
+		 (Alive,Dead,Alive,Alive,Alive);
+		 (Dead,Alive,Alive,Alive,Alive);
+		 (Alive,Alive,Dead,Dead,Alive);
+		 (Alive,Dead,Alive,Dead,Alive);
+		 (Dead,Alive,Alive,Dead,Alive);
+		 (Alive,Dead,Dead,Alive,Alive);
+		 (Dead,Alive,Dead,Alive,Alive);
+		 (Dead,Dead,Alive,Alive,Alive);
+		 (Alive,Dead,Dead,Dead,Alive);
+		 (Dead,Alive,Dead,Dead,Alive);
+		 (Dead,Dead,Alive,Dead,Alive);
+		 (Dead,Dead,Dead,Alive,Alive);
+		 (Dead,Dead,Dead,Dead,Alive)]
+;;
 
+(*Vérifie si une règle est stable*)
+let isStabeRule (r:rule) = match r with (n, e, s, o, c) -> c = Alive;;
+
+let print_state (s:state) = if s = Alive then print_string "A" else print_string "D";;
+
+let print_rules (r:rule) =
+  match r with (a, b, c, d, e) ->
+    print_string"[";
+    print_state a;
+    print_state b;
+    print_state c;
+    print_state d;
+    print_state e;
+    print_string"]"
+;;
+
+(*Retire les règles stables de l'automate des règles de base*)
+let triBaseRules (l:automaton) =
+  (*Copie de baseRules*)
+  let br = ref !baseRules in
+  (*Suppression des règles stables l*)
+  let rec deleteRule l =
+    match l with
+    |[] -> !br
+    |h :: t -> 
+       (*parcours la copie de baseRules et supprime la règle h de br*)
+       let rec aux headofrules list headofbr br=
+	 match list with
+	 |[] -> ()
+	 |h :: t -> 
+	    if h = headofrules then br := (headofbr@t)
+	    else (aux headofrules t (h::headofbr) br)
+       in aux h (!br) [] br;
+       deleteRule t
+  in deleteRule l
+;;
+
+(*Fonctions annexes*)
+(*Converti une formula en string*)
 let rec string_of_formule = 
 function
 |True -> "Vrai"
@@ -20,54 +76,14 @@ function
 |Neg y -> " Neg " ^ "(" ^ (string_of_formule y) ^ ")"
 ;;
 
-let rec desc_n f = 
-match f with
-|Neg(Neg x) -> desc_n x
-|Neg(And(x, y)) -> Or((desc_n x),(desc_n y))
-|Neg(Or(x, y)) -> And((desc_n x),(desc_n y))
-|And(x, y) -> And((desc_n x),(desc_n y))
-|Or(x, y) -> Or((desc_n x),(desc_n y))
-|_ -> f;;
-
-let rec desc_ou f = 
-match f with 
-|And(x, y) -> And((desc_ou x),(desc_ou y))
-|Or(x, y) -> let g1 = (desc_ou x) and g2 = (desc_ou y) 
-	     in match (g1, g2) with 
-	     |And(a, b), _ ->
-		And((desc_ou (Or(a, g2)),(desc_ou (Or(b, g2)))))
-;;
-
-let fnc f = desc_ou(desc_n f);;
-
+(*Retourne l'id de la case dans la position demandée*)
 let getId d i j = (d*i+j+1);;
 let getNorth d i j = if i = 0 then (d*(d-1)+j+1) else (d*(i-1)+j+1);;
 let getSouth d i j = if i = (d-1) then (j+1) else (d*(i+1)+j+1);;
 let getEast d i j = if j = (d-1) then (d*i+1) else (d*i+j+1+1);;
 let getWest d i j = if j = 0 then (d*i+(d-1)+1) else (d*i+j-1+1);;
 
-let fold_and  l =
-  let rec aux l f =
-    match l with
-    |[] -> f
-    |h :: t -> aux t (And(h,f))
-  in aux l True
-;;
-
-let fold_or  l =
-  let rec aux l f =
-    match l with
-    |[] -> f
-    |h :: t -> aux t (Or(h,f))
-  in aux l False
-;;
-
-let rec print_list l =
-  match l with
-  |[] -> print_string "Vide"
-  |h :: t ->print_string "a"; print_string(string_of_formule h); print_list t
-;;
-
+(*Retourne la variable en fonction de l'état de la case*)
 let getVar (s:state) i =
   match s with
   |Alive -> Var(i)
@@ -76,21 +92,24 @@ let getVar (s:state) i =
 
 (*Converti une règle en formule*)
 let stables (a:automaton) d =
-  let list = ref [] in
+  let f = ref True in
   for i=0 to d do
     for j=0 to d do 
-      let rec rules a i j d f =
-	match a with
-	|[] -> f
-	|(n, e, s, o, c) :: t ->
-	   let l = [getVar n (getNorth d i j);
-		    getVar e (getEast d i j);
-		    getVar s (getSouth d i j);
-		    getVar o (getWest d i j);
-		    getVar c (getId d i j)] in
-	   (rules t i j d ((fold_and l)::f))
-      in list := (fold_or (rules a i j d []))::!list;
+      let rec rules r d i j f =
+	match r with
+	|[] -> !f
+	|(n,e,s,o,c)::t ->
+	   f := And(Or(Neg(getVar n (getNorth d i j)),
+		       Or(Neg(getVar e (getEast d i j)),
+			  Or(Neg(getVar s (getSouth d i j)),
+			     Or(Neg(getVar o (getWest d i j)),
+				getVar c (getId d i j))))),!f);
+	  rules t d i j f
+      in f := And((rules (triBaseRules a) d i j (ref True)),!f);
     done;
   done;
-  (fold_and !list);
+  print_string (string_of_formule (!f));
+  !f
 ;;
+
+
