54c03a5489a2349174eab512e12db6677b9713bb
MaxouGJ <mgallaisj@gmail.com>
2015-12-21 18:14:17 +0100

    Toutes les méthodes permettant de générer les générations stables

--- stable.ml
+++ stable.ml
@@ -1,8 +1,13 @@
 open Automaton;;
-open Logic
+open Logic;;
+open Str;;
 
+(*Exception lancée lorsque sortie renvoie SAT mais qu'il n'y a pas d'affectaction derrière*)
 exception Sat_exception;;
+(*Exception lancée lorsque sortie renvoie UNSAT*)
 exception Unsat_exception;;
+(*Exception lancée lorsque minisat échoue*)
+exception Minisat_exception;;
 
 (*Ajoute s à la fin d'entree.dimacs*)
 let add_dimacs s =
@@ -11,15 +16,14 @@ let add_dimacs s =
   close_out fic;
 ;;
 
-(*Renvoie une liste de toutes les variables du string s*)
-let split s =
-  let rec aux s v l =
-    if String.length s = 1 then l (*Pour ne pas récupérer le 0 de fin on s'arrête un caractère avant*)
-      else
-	match s with
-	|" " -> aux (String.sub s 1 ((String.length s)-1)) "" (v::l)
-	|x -> aux (String.sub s 1 ((String.length s)-1)) (v^x)  l
-  in List.rev (aux s "" [])
+(*Inverse le sens de la liste l et donne la négation de chaque variable*)
+let list_neg l =
+  let rec list_neg_rec l s =
+      match l with
+      |[] | [_] -> (s ^ " 0")
+      |h :: t -> if String.get h 0 = '-' then list_neg_rec t (s ^" "^ (String.sub h 1 ((String.length h) -1)))
+	  else list_neg_rec t (s ^ " -" ^h)
+  in list_neg_rec l ""	  
 ;;
 
 (*Récupère les variables du résultat de minisat et les ajoute dans entree.dimacs*)
@@ -30,13 +34,21 @@ let recup_var f =
     with
       End_of_file -> None
   in match line with
-  |Some x -> add_dimacs x; split x
-  |None -> raise Sat_exception 
+  |Some x -> let l = Str.split (Str.regexp(" ")) x in add_dimacs (list_neg l); l
+  |None -> print_string "La formule est satisfaisable mais minisat n'a réussi aucune affectation"; raise Sat_exception 
 ;;
 
-(*Affiche l'automate stable de dimension d comportant les valeurs des cariables dans l*)
+(*Affiche l'automate stable de dimension d comportant les valeurs des variables dans l*)
 let affiche_stable l d = 
-  print_string "\n--------------------\n";
+  let g = Array.make_matrix d d Dead in
+  let rec constr l g i j d=
+    match l with
+    |[] | [_] -> () (*Pour ne pas traiter le 0 de fin*)
+    |h :: t -> if String.get h 0 != '-' then  g.(i).(j) <- Alive;
+      if j= (d-1) then constr t g (i+1) 0 d
+      else constr t g i (j+1) d
+  in constr l g 0 0 d;
+  show_generation g;
 ;;
 
 (*Parse le fichier entree.dimacs*)
@@ -48,38 +60,34 @@ let parse_dimacs d =
     with
       End_of_file -> None
   in match line with
-  |Some ("UNSAT") -> raise Unsat_exception
   |Some ("SAT") -> affiche_stable (recup_var fic) d;
-  |Some (_) -> raise Format_non_standard
-  |None -> raise Format_non_standard
+  |Some ("UNSAT") -> print_string "Aucune génération stable trouvée.\n"; raise Unsat_exception
+  |Some (_) -> print_string "Erreur lors de l'exécution de minisat.\n"; raise Minisat_exception
+  |None -> print_string "Erreur lors de l'exécution de minisat.\n"; raise Minisat_exception
 ;;
 
+(*Boucle répétée pour avoir autant de formule stable que demandé*)
 let rec boucle d =
-  match (parse_dimacs d)  with
-  (*|Unsat_exception -> print_string "Il n'y a plus de génération stable à afficher.\n"
-    |Format_non_standard -> print_string "Erreur lors de l'execution de minisat.\n"*)
-  |_ -> print_string "Presser :\n\t-q pour quitter\n\t-entrée pour afficher une autre génération stable\n";
-    begin
-      match read_line() with
-      |"" -> boucle d
-      |"q" -> print_string "Au revoir.\n"
-      |_ -> print_string "Vous n'avez pas le droit à l'erreur bye.\n"
-    end
+  if (Sys.command "minisat entree.dimacs sortie") = 1
+  then print_string "Echec de minisat.\n";
+  let dimacs = 
+    try Some(parse_dimacs d)
+    with
+    |Unsat_exception -> None
+    |Minisat_exception -> None
+  in match dimacs with
+  |None -> ()
+  |_ -> 
+     print_string "Presser la touche :
+\n\t-q pour quitter
+\n\t-entrée pour afficher une autre génération stable\n";
+    match read_line() with
+    |"q" -> print_string "Au revoir.\n"
+    |_ -> boucle d
 ;;
 
 (*Permet de savoir si l'automate a de taille d a au moins une configuration stable*)
 let show_stable (a:automaton) d =
   create_dimacs (stables a d);
-  if (Sys.command "minisat entree.dimacs sortie") = 1
-  then print_string "Echec de minisat";
-  match (parse_dimacs d) with
-  (*|Unsat_exception -> print_string "Il n'y a pas de configuration stable pour cette automate.\n"
-    |Format_non_standard -> "Erreur lors de l'execution de minisat.\n"*)
-  |_ -> print_string "Presser :\n\t-q pour quitter\n\t-entrée pour afficher une autre génération stable\n";
-    begin
-      match read_line() with
-      |"" -> boucle d
-      |"q" -> print_string "Au revoir.\n"
-      |_ -> print_string "Vous n'avez pas le droit à l'erreur bye.\n"
-    end
+  boucle d;
 ;;
