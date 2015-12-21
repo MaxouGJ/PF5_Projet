b6779797ddf3ccbc2fc1980ce466b7e03dfa4b17
MaxouGJ <mgallaisj@gmail.com>
2015-12-21 18:12:44 +0100

    Interface de logic.ml

--- logic.mli
+++ logic.mli
@@ -11,10 +11,10 @@ type formula =
 val baseRules : Automaton.rule list ref
 
 (*Retire les règles stables de l'automate des règles de base*)
-val triBaseRules : Automaton.automaton -> Automaton.rule list
+val tri_base_rules : Automaton.automaton -> Automaton.rule list
 
-(*Converti une règle en formule*)
+(*Convertit une règle en formule*)
 val stables : Automaton.automaton -> int -> formula * int * int
 
-(*Converti une formule en fichier dimacs*)
+(*Convertit une formule en fichier dimacs*)
 val create_dimacs : formula * int * int -> unit
